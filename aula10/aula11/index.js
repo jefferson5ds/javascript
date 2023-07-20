88a667a64f2981d235a6e88c99c3d623ddaebb4a
JEFFERSON <123278900+jefferson5ds@users.noreply.github.com>
2023-07-20 20:51:57 -0300

    .

---
+++ aula10/aula11/index.js
@@ -0,0 +1,6 @@
+var vel= 6
+console.log(`à velocidade do seu carro é ${vel}km/h`);
+if(vel>60){ // condição simples 
+   console.log(`voce ultrapassou a velocidade permitida. multado!`)
+}
+console.log(`dirija sempore usando o sinto de segurança!`)
