ec8137d172038a925c1177caf743fd7d5b98a824
JEFFERSON <123278900+jefferson5ds@users.noreply.github.com>
2023-07-21 21:11:48 -0300

    .

--- aula10/aula13/modelo/script.js
+++ aula10/aula13/modelo/script.js
@@ -1,6 +1,6 @@
 function carregar(){
-var msg = window.document.getElementById('msg');
-var imagem = window.document.getElementById('imagem');
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 
