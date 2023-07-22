5b3b48a830303d6c6a7b307545e3f0ffd6efa147
JEFFERSON <123278900+jefferson5ds@users.noreply.github.com>
2023-07-21 21:00:40 -0300

    .

--- aula10/aula13/modelo/script.js
+++ aula10/aula13/modelo/script.js
@@ -1,1 +1,27 @@
-alert('olá')
+function carregar(){
+var msg = window.document.getElementById('msg');
+var imagem = window.document.getElementById('imagem');
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    img.src ='manha.jpg'
+  }else if(hora >= 12 && hora <=18) {
+      img.src ='tarde.jpg'
+  }else {
+      img.src ='noite.jpg'
+  } 
+
+
+
+}
+
+
+
+
+
+
+
+
+
