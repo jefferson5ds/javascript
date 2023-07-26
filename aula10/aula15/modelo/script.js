3beb3a53b83ce3aec656eb7b13083be966e610a9
JEFFERSON <123278900+jefferson5ds@users.noreply.github.com>
2023-07-26 10:53:10 -0300

    .

--- aula10/aula15/modelo/script.js
+++ aula10/aula15/modelo/script.js
@@ -1,4 +1,30 @@
+function contar(){
+    var ini = window.document.getElementById('txti');
+    var fim = window.document.getElementById('txtf');
+    var passo = window.document.getElementById('txtp');
+    let res = window.document.getElementById('res');
 
+    if (ini.value.lenght  == 0  || fim.value.lenght == 0 || passo.value.lenght == 0 ){
+       window.alert('[erro] faltam dados')
+       res.innerHTML = 'impossivel contar!'
+    }else{
+         res.innerHTML = 'Contando'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c= i; c <= f ; c += p ){
+            res.innerHTML += `${c}`
+        }
+
+    }
+    
+    
+
+
+
+
+}
 
 
 
